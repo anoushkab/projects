6498427cc3644d3928d9a1b37eeb5db57660ba99
anoushkab <60457752+anoushkab@users.noreply.github.com>
2020-03-05 14:54:30 -0500

    Create lil_alchemy.cpp

---
+++ lil_alchemy.cpp
@@ -0,0 +1,14 @@
+#include "Game.h"
+#include "Element.h"
+#include "Alchemist.h"
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main () {
+  Game newGame;
+  newGame.StartGame();
+  return 0;
+}
